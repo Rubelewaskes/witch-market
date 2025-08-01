18158083cb4156621a1af31afe2452ea1b752602
Rubelewaskes <sogrian@gmail.com>
2025-08-01 13:47:45 +0300

    Added Scholar Service in project

---
+++ scholar-service/src/main/java/com/witchshop/scholarservice/ScholarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.witchshop.scholarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScholarServiceApplication.class, args);
+    }
+
+}
