9356a15aad6332bd7f3b3f816487cd00a95d87ef
Rubelewaskes <sogrian@gmail.com>
2025-08-01 13:50:35 +0300

    Added Potion Service in project

---
+++ potion-service/src/test/java/com/witchshop/potionservice/PotionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witchshop.potionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
