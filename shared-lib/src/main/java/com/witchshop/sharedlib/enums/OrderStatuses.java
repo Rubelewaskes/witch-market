a12de8b3aa30223a89c2a80a168e9849beaf89fd
Rubelewaskes <sogrian@yandex.ru>
2025-08-12 17:47:32 +0300

    Перенесены Enumы

---
+++ shared-lib/src/main/java/com/witchshop/sharedlib/enums/OrderStatuses.java
@@ -0,0 +1,8 @@
+package com.witchshop.sharedlib.enums;
+
+public enum OrderStatuses {
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
+
---
+++ shared-lib/src/main/java/com/witchshop/sharedlib/enums/Specialization.java
@@ -0,0 +1,9 @@
+package com.witchshop.sharedlib.enums;
+
+public enum Specialization {
+    ARTIFACT,
+    CHIMEROLOGIST,
+    POTION,
+    ARTIFACTOR, SCHOLAR
+}
+
