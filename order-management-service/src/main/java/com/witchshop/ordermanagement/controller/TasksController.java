e97bcbe2660b05c9fef8449757e8e2463b177f50
Rubelewaskes <sogrian@gmail.com>
2025-08-17 17:31:49 +0300

    Фикс замечаний

--- order-management-service/src/main/java/com/witchshop/ordermanagement/controller/TasksController.java
+++ order-management-service/src/main/java/com/witchshop/ordermanagement/controller/TasksController.java
@@ -5,7 +5,6 @@ import com.witchshop.ordermanagement.service.CoordinatorService;
 import com.witchshop.sharedlib.dao.Workers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
