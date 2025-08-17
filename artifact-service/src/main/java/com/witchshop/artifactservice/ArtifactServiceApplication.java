f8eddfdfe8504aea0ebd81de31fbf7fa0a0aa5cb
Rubelewaskes <sogrian@gmail.com>
2025-08-17 17:29:45 +0300

    Создан эндпоинт на основе shared lib

--- artifact-service/src/main/java/com/witchshop/artifactservice/ArtifactServiceApplication.java
+++ artifact-service/src/main/java/com/witchshop/artifactservice/ArtifactServiceApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@MapperScan("com.witchshop.artifactservice.mapper")
+@MapperScan({"com.witchshop.artifactservice.mapper",
+             "com.witchshop.sharedlib.mapper"})
 @EnableScheduling
 public class ArtifactServiceApplication {
     public static void main(String[] args) {
