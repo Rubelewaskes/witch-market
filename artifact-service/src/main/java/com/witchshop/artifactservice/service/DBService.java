ed9c555bc2df8f8a21a21d8c51b8c59da346b273
Rubelewaskes <sogrian@gmail.com>
2025-08-18 19:30:52 +0300

    Создан конфиг hazelcast и запущено кеширование получения возможных действий по специализации и кеширование воркеров в артифакт сервисе

--- artifact-service/src/main/java/com/witchshop/artifactservice/service/DBService.java
+++ artifact-service/src/main/java/com/witchshop/artifactservice/service/DBService.java
@@ -8,6 +8,8 @@ import com.witchshop.sharedlib.enums.Specialization;
 import com.witchshop.sharedlib.enums.TaskStatuses;
 import com.witchshop.sharedlib.mapper.PipelineStepDefinitionMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,8 +22,19 @@ public class DBService {
     private final TaskExecutionMapper taskExecutionMapper;
     private final PipelineStepDefinitionMapper pipelineStepDefinitionMapper;
 
+    @Cacheable(value="workers")
     public Workers selectWorker() {
         return workerMapper.findAvailableWorker();
+    }
+
+    @CacheEvict(value = "workers", allEntries = true)
+    public void insertWorker(Workers worker) {
+        workerMapper.insertWorker(worker);
+    }
+
+    @CacheEvict(value = "workers", allEntries = true)
+    public void updateWorkerActivity(UUID workerId) {
+        workerMapper.updateActivity(workerId);
     }
 
     public void updateWorkerInTask(UUID workerId, UUID taskId) {
@@ -29,12 +42,15 @@ public class DBService {
     }
 
     public void updateTaskStatus(UUID id, TaskStatuses status) {
+
         taskExecutionMapper.updateTaskStatus(id, status);
     }
 
     public void updateTaskAfterEnd(UUID id, TaskStatuses status, String result) {
         taskExecutionMapper.updateTaskAfterEnd(id, status, result);
     }
+
+    @Cacheable(value="pipeline_step_definitions")
     public List<PipelineStepDefinition> selectBlueprints(){
         return pipelineStepDefinitionMapper.selectStepsBySpecialization(Specialization.ARTIFACT);
     }
